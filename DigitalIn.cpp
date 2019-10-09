705e55e85ec606f3da7507264251dd7c459dfd69
Dan <doates625@gmail.com>
2019-10-09 16:22:36 -0400

    Tested for both Arduino and Mbed platforms

---
+++ DigitalIn.cpp
@@ -0,0 +1,65 @@
+/**
+ * @brief DigitalIn.cpp
+ * @author Dan Oates (WPI Class of 2020)
+ */
+#include "DigitalIn.h"
+
+#if defined(PLATFORM_ARDUINO)
+
+/**
+ * @brief Constructs digital input
+ * @param pin Platform-specific pin ID
+ * @param pull Pin mode (PullUp, PullNone)
+ */
+DigitalIn::DigitalIn(PLATFORM_PIN_TYPE pin, PinMode pull)
+{
+	this->pin = pin;
+	mode(pull);
+}
+
+/**
+ * @brief Reads digital input
+ * @return 1 if high, 0 if low
+ */
+int DigitalIn::read()
+{
+	return digitalRead(pin);
+}
+
+/**
+ * @brief Sets pin mode
+ * @param pull Pin mode (PullUp, PullNone)
+ */
+void DigitalIn::mode(PinMode pull)
+{
+	switch (pull)
+	{
+		case PullUp:
+			pinMode(pin, INPUT_PULLUP);
+			break;
+		case PullNone:
+			pinMode(pin, INPUT);
+			break;
+	}
+}
+
+/**
+ * @brief Returns 1 if object is connected to input
+ * 
+ * Note: For Arduino, this always returns 1.
+ */
+int DigitalIn::is_connected()
+{
+	return 1;
+}
+
+/**
+ * @brief Shorthand for reading input
+ * @return 1 if high, 0 if low
+ */
+DigitalIn::operator int()
+{
+	return read();
+}
+
+#endif
---
+++ DigitalIn.h
@@ -0,0 +1,47 @@
+/**
+ * @file DigitalIn.h
+ * @brief Class for Arduino and Mbed digital inputs
+ * @author Dan Oates (WPI Class of 2020)
+ * 
+ * This class implements the Mbed DigitalIn class for Arduino to allow for
+ * cross-platform libraries. Additionally, the type PinMode from Mbed is defined
+ * in this file. For the Mbed platform, PinMode can be one of (PullUp, PullDown,
+ * PullNone, or OpenDrain). For Arduino, only PullUp and PullNone are supported.
+ * 
+ * Dependencies:
+ * - Platform: https://github.com/doates625/Platform.git
+ * 
+ * References:
+ * - Mbed DigitalIn: https://os.mbed.com/docs/mbed-os/v5.14/apis/digitalin.html
+ */
+#pragma once
+#include <Platform.h>
+
+#if defined(PLATFORM_ARDUINO)
+
+/**
+ * PinMode Enumeration
+ */
+typedef enum
+{
+	PullUp,
+	PullNone,
+}
+PinMode;
+
+/**
+ * Class Declaration
+ */
+class DigitalIn
+{
+public:
+	DigitalIn(PLATFORM_PIN_TYPE pin, PinMode mode = PullNone);
+	int read();
+	void mode(PinMode mode);
+	int is_connected();
+	operator int();
+private:
+	PLATFORM_PIN_TYPE pin;
+};
+
+#endif
