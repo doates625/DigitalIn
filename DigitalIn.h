31799587a5490f482167bd580badefa30657ab08
Dan <doates625@gmail.com>
2019-10-18 11:59:58 -0700

    Moved description to README

--- DigitalIn.h
+++ DigitalIn.h
@@ -2,17 +2,6 @@
  * @file DigitalIn.h
  * @brief Class for Arduino and Mbed digital inputs
  * @author Dan Oates (WPI Class of 2020)
- * 
- * This class implements the Mbed DigitalIn class for Arduino to allow for
- * cross-platform libraries. Additionally, the type PinMode from Mbed is defined
- * in this file. For the Mbed platform, PinMode can be one of (PullUp, PullDown,
- * PullNone, or OpenDrain). For Arduino, only PullUp and PullNone are supported.
- * 
- * Dependencies:
- * - Platform: https://github.com/doates625/Platform.git
- * 
- * References:
- * - Mbed DigitalIn: https://os.mbed.com/docs/mbed-os/v5.14/apis/digitalin.html
  */
 #pragma once
 #include <Platform.h>
---
+++ README.md
@@ -0,0 +1,12 @@
+# DigitalIn
+Class for Arduino and Mbed digital inputs  
+Written by Dan Oates (WPI Class of 2020)
+
+### Description
+This class implements the Mbed DigitalIn class for Arduino to allow for cross-platform libraries. Additionally, the type PinMode from Mbed is defined in this file. For the Mbed platform, PinMode can be one of (PullUp, PullDown, PullNone, or OpenDrain). For Arduino, only PullUp and PullNone are supported.
+
+### Dependencies
+- [Platform](https://github.com/doates625/Platform.git)
+
+### References
+- [Mbed DigitalIn](https://os.mbed.com/docs/mbed-os/v5.14/apis/digitalin.html)
